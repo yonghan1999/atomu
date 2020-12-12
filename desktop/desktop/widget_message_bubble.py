85cbb6687e3df36e5c7b994a55a19c4d4f827ae0
yuuki <yong.han.life@outlook.com>
2020-12-13 04:00:12 +0800

    desktop/widget/message_bubble: add new widget

---
+++ desktop/desktop/widget_message_bubble.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from gi.repository import Gtk, Gio, GLib, Gdk
+
+@Gtk.Template(resource_path="/com/atomu/client/desktop/ui/widget_message_bubble.ui")
+class MessageBubbleRow(Gtk.Box):
+    __gtype_name__ = 'MessageBubbleRow'
+
+    text = Gtk.Template.Child()
+    avatar = Gtk.Template.Child()
+
+    def __init__(self, uid, **kwargs):
+        super().__init__(**kwargs)
+        self.uid = uid
+
+    def reverse(self):
+        print(self)
+        print(self.text)
+        self.child_set_property(self.text, "pack-type", Gtk.PackType.END)
+        self.avatar.destroy()
